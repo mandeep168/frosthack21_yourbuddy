d4d562b03cb1a89fb647ad0c7aad5e770cf31b80
mandeep168 <64517334+mandeep168@users.noreply.github.com>
2021-05-09 12:20:30 +0530

    Update home.php

--- home.php
+++ home.php
@@ -21,7 +21,7 @@ if(!isset($_SESSION['username'])){
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="home.css">
+<link rel="stylesheet" type="text/css" href="home.css">
 </head>
 
 <body>
@@ -33,10 +33,10 @@ if(!isset($_SESSION['username'])){
     </label>
         <label class="logo">YourBuddy</label>
         <ul>
-            <li><a class="active" href=#>Home</a></li>
-            <li><a href="aboutus.html">AboutUs</a></li>
+            <li><a class="active" href="http://localhost/log/home.php">Home</a></li>
+            <li><a href="http://localhost/log/aboutus.html">AboutUs</a></li>
             <li><a href="http://localhost/log/profile.php">Profile</a></li>
-            <li><a href="logout.php">Logout</a></li>
+            <li><a href="http://localhost/log/logout.php">Logout</a></li>
         </ul>
     <div class="jumbotron color"> 
 
@@ -50,11 +50,47 @@ if(!isset($_SESSION['username'])){
   <hr class="my-4">
   <p>Emi is always there to help you.</p>
   <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-
+   
   </p>
 </div>
     </nav>
+
+
+<div class="container">
+        <div class="row">
+            <div class="col-sm-4 py-3 py-sm-0">
+                <div class="card box-shadow" >
+                    <div class="card-body">
+                        <h5 class="card-title">Challenge 1</h5>
+                        <p class="card-text">This challenge contain amazing tasks.</p>
+                        <a href="challenge1.php" class="btn btn-primary">Let's Go</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4 py-3 py-sm-0">
+                <div class="card box-shadow" >
+                    <div class="card-body">
+                        <h5 class="card-title">Challenge 2</h5>
+                        <p class="card-text">This challenge contain some exercises.</p>
+                        <a href="challenge2.php" class="btn btn-primary">Let's Go</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4 py-3 py-sm-0">
+                <div class="card box-shadow">
+                    <div class="card-body">
+                        <h5 class="card-title">Challenge 3</h5>
+                        <p class="card-text">This challenge contain some amazing .</p>
+                        <a href="#" class="btn btn-primary">Let's Go</a>
+                    </div>
+                </div>
+            </div>            
+        </div>
+    </div>
+
+
 </body>
 </html>
 
